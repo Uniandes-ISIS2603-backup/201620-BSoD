d929ae642f5f9d69011d0053b760eddd935fae29
cc.novoa11 <cc.novoa11@ISIS2603-70.sis.virtual.uniandes.edu.co>
2016-09-13 09:00:23 -0500

    CRUD domicilio (no carga direccion)

--- src/main/webapp/src/modules/domicilios/domicilios.ctrl.js
+++ src/main/webapp/src/modules/domicilios/domicilios.ctrl.js
@@ -1,72 +1,89 @@
 (function (ng) {
     var mod = ng.module("domiciliosModule");
 
-    mod.controller("domiciliosCtrl", ['$scope', '$state', '$stateParams', '$http', 'domicilioContext', function ($scope, $state, $stateParams, $http, context) {
+        mod.controller("domiciliosCtrl", ['$scope', '$state', '$stateParams', '$http', 'domicilioContext', function ($scope, $state, $stateParams, $http, context) {
 
-            // inicialmente el listado de domicilios está vacio
+            // inicialmente el listado de domicilios estÃ¡ vacio
             $scope.records = {};
-            // carga los domicilios
-            $http.get(context).then(function (response) {
-                $scope.records = response.data;
+            // carga domicilios
+            $http.get(context).then(function(response){
+                $scope.records = response.data;    
             }, responseError);
 
-            // el controlador recibió un domicilioID
-            // revisa los parámetros
+            // el controlador recibiÃ³ un domicilioId ??
+            // revisa los parÃ¡metros (ver el :domicilioId en la definiciÃ³n de la ruta)
             if ($stateParams.domicilioId !== null && $stateParams.domicilioId !== undefined) {
-
-                // toma el id del parámetro
+                
+                // toma el id del parÃ¡metro
                 id = $stateParams.domicilioId;
                 // obtiene el dato del recurso REST
                 $http.get(context + "/" + id)
-                        .then(function (response) {
-                            // $http.get es una promesa
-                            // cuando llegue el dato, actualice currentRecord
-                            $scope.currentRecord = response.data;
-                        }, responseError);
+                    .then(function (response) {
+                        // $http.get es una promesa
+                        // cuando llegue el dato, actualice currentRecord
+                        $scope.currentRecord = response.data;
+                    }, responseError);
 
-                // el controlador no recibió un domicilioId
+            // el controlador no recibiÃ³ un domicilioId
             } else
             {
                 // el registro actual debe estar vacio
                 $scope.currentRecord = {
                     id: undefined /*Tipo Long. El valor se asigna en el backend*/,
-                    name: '' /*Tipo String*/
+                    direccion: '' /*Tipo String*/,
+                    plato:'',
+                    precio: undefined
                 };
-
+              
                 $scope.alerts = [];
             }
 
 
             this.saveRecord = function (id) {
                 currentRecord = $scope.currentRecord;
-
+                
                 // si el id es null, es un registro nuevo, entonces lo crea
                 if (id == null) {
 
                     // ejecuta POST en el recurso REST
                     return $http.post(context, currentRecord)
-                            .then(function () {
-                                // $http.post es una promesa
-                                // cuando termine bien, cambie de estado
-                                $state.go('domiciliosList');
-                            }, responseError);
-
-                    // si el id no es null, es un registro existente entonces lo actualiza
+                        .then(function () {
+                            // $http.post es una promesa
+                            // cuando termine bien, cambie de estado
+                            $state.go('domiciliosList');
+                        }, responseError);
+                        
+                // si el id no es null, es un registro existente entonces lo actualiza
                 } else {
-
+                    
                     // ejecuta PUT en el recurso REST
                     return $http.put(context + "/" + currentRecord.id, currentRecord)
-                            .then(function () {
-                                // $http.put es una promesa
-                                // cuando termine bien, cambie de estado
-                                $state.go('domiciliosList');
-                            }, responseError);
-                }
-                ;
+                        .then(function () {
+                            // $http.put es una promesa
+                            // cuando termine bien, cambie de estado
+                            $state.go('domiciliosList');
+                        }, responseError);
+                };
             };
 
+           this.deleteRecord = function (id) {
+                currentRecord = $scope.currentRecord;
+                if(id!=null)
+                {            
+                    // ejecuta delete en el recurso REST
+                    return $http.delete(context + "/" + id,currentRecord)
+                        .then(function () {
+                            $scope.records = {};
+                            $http.get(context).then(function(response){
+                                $scope.records = response.data;    
+                            }, responseError);
+                            $state.go('domiciliosList');
+                        }, responseError); 
+                }
+                };
+
             // -----------------------------------------------------------------
-            // Funciones para manejra los mensajes en la aplicación
+            // Funciones para manejra los mensajes en la aplicaciÃ³n
 
 
             //Alertas
@@ -74,7 +91,7 @@
                 $scope.alerts.splice(index, 1);
             };
 
-            // Función showMessage: Recibe el mensaje en String y su tipo con el fin de almacenarlo en el array $scope.alerts.
+            // FunciÃ³n showMessage: Recibe el mensaje en String y su tipo con el fin de almacenarlo en el array $scope.alerts.
             function showMessage(msg, type) {
                 var types = ["info", "danger", "warning", "success"];
                 if (types.some(function (rc) {
@@ -100,4 +117,3 @@
         }]);
 
 })(window.angular);
-
