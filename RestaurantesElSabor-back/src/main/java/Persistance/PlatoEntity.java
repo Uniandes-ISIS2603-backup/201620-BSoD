4fca3a717ccb615b0ffbd59d2422fce691ba98f0
Zulma Castañeda <zl.castaneda10@uniandes.edu.co>
2016-10-06 11:47:44 -0500

    Creacion del paquete
    Creen las clases aqui

---
+++ RestaurantesElSabor-back/src/main/java/Persistance/PlatoEntity.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persistance;
+
+import co.edu.uniandes.bsod.restauranteselsabor.entities.BaseEntity;
+
+/**
+ *
+ * @author zl.castaneda10
+ */
+public class PlatoEntity extends BaseEntity{
+    
+}
