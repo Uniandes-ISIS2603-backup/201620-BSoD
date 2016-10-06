ade7445c4539f3a56cfd871a7b330d122102d82f
Zulma Castañeda <zl.castaneda10@uniandes.edu.co>
2016-10-06 12:02:23 -0500

    pequeños cambios en el xml
    :dancer:

--- RestaurantesElSabor-back/src/main/java/co/edu/uniandes/bsod/restauranteselsabor/entities/PlatoEntity.java
+++ RestaurantesElSabor-back/src/main/java/co/edu/uniandes/bsod/restauranteselsabor/entities/PlatoEntity.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Persistance;
+package co.edu.uniandes.bsod.restauranteselsabor.entities;
 
 import co.edu.uniandes.bsod.restauranteselsabor.entities.BaseEntity;
 import java.io.Serializable;
