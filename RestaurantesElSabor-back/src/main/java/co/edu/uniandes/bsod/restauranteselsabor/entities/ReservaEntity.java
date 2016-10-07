db8c5ce60f15eea775b4c29292d6b78a8fab0639
aj.paredes10 <aj.paredes10@uniandes.edu.co>
2016-10-07 11:32:00 -0500

    reservaEntity

---
+++ RestaurantesElSabor-back/src/main/java/co/edu/uniandes/bsod/restauranteselsabor/entities/ReservaEntity.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.bsod.restauranteselsabor.entities;
+
+/**
+ *
+ * @author aj.paredes10
+ */
+public class ReservaEntity {
+    
+}
