d873a9d57363b42652216373cd6d0f82af12dd2d
Zulma Castañeda <zl.castaneda10@uniandes.edu.co>
2016-10-08 19:47:48 -0500

    cambio XML
    creacion plato entity

--- RestaurantesElSabor-back/src/main/java/co/edu/uniandes/bsod/restauranteselsabor/entities/PlatoEntity.java
+++ RestaurantesElSabor-back/src/main/java/co/edu/uniandes/bsod/restauranteselsabor/entities/PlatoEntity.java
@@ -5,13 +5,52 @@
  */
 package co.edu.uniandes.bsod.restauranteselsabor.entities;
 
-import co.edu.uniandes.bsod.restauranteselsabor.entities.BaseEntity;
+
 import java.io.Serializable;
+import javax.persistence.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
 
 /**
  *
  * @author zl.castaneda10
  */
+@Entity
 public class PlatoEntity extends BaseEntity implements Serializable{
+    
+    private int precio;
+    private String descripcion;
+    @OneToMany( mappedBy = "sucursal")
+    private SucursalEntity sucursal;
+    
+    public int getPrecio() {
+        return precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public SucursalEntity getSucursal() {
+        return sucursal;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setSucursal(SucursalEntity sucursal) {
+        this.sucursal = sucursal;
+    }
+    
+    
+    
     
 }
