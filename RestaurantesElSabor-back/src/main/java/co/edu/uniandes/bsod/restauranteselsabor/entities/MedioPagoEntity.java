fde95f74ae7e9fbc5a80a842449cc31154350ee9
aj.paredes10 <aj.paredes10@uniandes.edu.co>
2016-10-09 22:57:21 -0500

    reservaEntity, medioPagoEntity

--- RestaurantesElSabor-back/src/main/java/co/edu/uniandes/bsod/restauranteselsabor/entities/MedioPagoEntity.java
+++ RestaurantesElSabor-back/src/main/java/co/edu/uniandes/bsod/restauranteselsabor/entities/MedioPagoEntity.java
@@ -5,6 +5,8 @@
  */
 package co.edu.uniandes.bsod.restauranteselsabor.entities;
 
+import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.*;
 import uk.co.jemos.podam.common.PodamExclude;
 /**
@@ -12,9 +14,79 @@ import uk.co.jemos.podam.common.PodamExclude;
  * @author aj.paredes10
  */
 @Entity
-public class MedioPagoEntity extends BaseEntity
+public class MedioPagoEntity extends BaseEntity implements Serializable
 {    
     @PodamExclude
     @ManyToOne
     ClienteEntity cliente;
+    
+    private Integer efectivo;
+
+    private String tarjeta;
+
+    private Long numerosTarjeta;
+
+    private Date fechaVencimiento;
+
+    private Integer codigoSeguridad;
+
+    private String franquicia;
+
+    public ClienteEntity getCliente() {
+        return cliente;
+    }
+
+    public Integer getEfectivo() {
+        return efectivo;
+    }
+
+    public String getTarjeta() {
+        return tarjeta;
+    }
+
+    public Long getNumerosTarjeta() {
+        return numerosTarjeta;
+    }
+
+    public Date getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public Integer getCodigoSeguridad() {
+        return codigoSeguridad;
+    }
+
+    public String getFranquicia() {
+        return franquicia;
+    }
+
+    public void setCliente(ClienteEntity cliente) {
+        this.cliente = cliente;
+    }
+
+    public void setEfectivo(Integer efectivo) {
+        this.efectivo = efectivo;
+    }
+
+    public void setTarjeta(String tarjeta) {
+        this.tarjeta = tarjeta;
+    }
+
+    public void setNumerosTarjeta(Long numerosTarjeta) {
+        this.numerosTarjeta = numerosTarjeta;
+    }
+
+    public void setFechaVencimiento(Date fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public void setCodigoSeguridad(Integer codigoSeguridad) {
+        this.codigoSeguridad = codigoSeguridad;
+    }
+
+    public void setFranquicia(String franquicia) {
+        this.franquicia = franquicia;
+    }
+    
+    
 }
--- RestaurantesElSabor-back/src/main/java/co/edu/uniandes/bsod/restauranteselsabor/entities/ReservaEntity.java
+++ RestaurantesElSabor-back/src/main/java/co/edu/uniandes/bsod/restauranteselsabor/entities/ReservaEntity.java
@@ -5,6 +5,7 @@
  */
 package co.edu.uniandes.bsod.restauranteselsabor.entities;
 
+import java.util.Date;
 import javax.persistence.*;
 
 /**
@@ -20,5 +21,51 @@ public class ReservaEntity extends BaseEntity{
     @ManyToOne
     ClienteEntity cliente;
     
+    private Date fecha;
+    private int numPersonas;
+    private int piso;
+    
+    
+    
     // en sucursal iria @OneToMany(mappedBy="reservas")
+
+    public SucursalEntity getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(SucursalEntity sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    public ClienteEntity getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(ClienteEntity cliente) {
+        this.cliente = cliente;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getNumPersonas() {
+        return numPersonas;
+    }
+
+    public void setNumPersonas(int numPersonas) {
+        this.numPersonas = numPersonas;
+    }
+
+    public int getPiso() {
+        return piso;
+    }
+
+    public void setPiso(int piso) {
+        this.piso = piso;
+    }
 }
