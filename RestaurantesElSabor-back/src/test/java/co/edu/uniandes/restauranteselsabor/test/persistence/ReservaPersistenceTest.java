bdf6d304576c2d52e3ee6969a01bebeebfc8d002
aj.paredes10 <aj.paredes10@uniandes.edu.co>
2016-10-12 21:30:44 -0500

    reserva test

---
+++ RestaurantesElSabor-back/src/test/java/co/edu/uniandes/restauranteselsabor/test/persistence/ReservaPersistenceTest.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.restauranteselsabor.test.persistence;
+
+import co.edu.uniandes.bsod.restauranteselsabor.entities.ReservaEntity;
+import co.edu.uniandes.bsod.restauranteselsabor.persistence.ReservaPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author aj.paredes10
+ */
+@RunWith(Arquillian.class)
+public class ReservaPersistenceTest {
+    
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(ReservaEntity.class.getPackage())
+                .addPackage(ReservaEntity.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    @Inject
+    private ReservaPersistence reservaPersistence;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Inject
+    UserTransaction utx;
+
+    private List<ReservaEntity> data = new ArrayList<ReservaEntity>();
+    
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void setUp() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from ReservaEntity").executeUpdate();
+        em.createQuery("delete from SucursalEntity").executeUpdate();
+        em.createQuery("delete from ClienteEntity").executeUpdate();
+    }
+    
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     *
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 4; i++) {
+            ReservaEntity entity = factory.manufacturePojo(ReservaEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+    
+    /**
+     * Prueba para crear una Reserva.
+     */
+    @Test
+    public void createReservaTest() {
+        PodamFactory factory = new PodamFactoryImpl();
+        ReservaEntity newEntity = factory.manufacturePojo(ReservaEntity.class);
+
+        ReservaEntity result = reservaPersistence.create(newEntity);
+
+        Assert.assertNotNull(result);
+        ReservaEntity entity = em.find(ReservaEntity.class, result.getId());
+        Assert.assertNotNull(entity);
+        Assert.assertEquals(newEntity.getId(), entity.getId());
+    }
+    /**
+     * Prueba para consultar la lista de reservas.
+     */
+    @Test
+    public void getCompaniesTest() {
+        List<ReservaEntity> list = reservaPersistence.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for (ReservaEntity ent : list) {
+            boolean found = false;
+            for (ReservaEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+    
+    /**
+     * Prueba para consultar una reserva por id.
+     */
+    @Test
+    public void getReservaTest() {
+        ReservaEntity entity = data.get(0);
+        ReservaEntity newEntity = reservaPersistence.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getId(), newEntity.getId());
+    }
+    /**
+     * Prueba para consultar una Reserva por fecha.
+     */
+    @Test
+    public void getReservaByDateTest() {
+        ReservaEntity entity1 = data.get(0);
+        List<ReservaEntity> list = reservaPersistence.findByDate(entity1.getFecha());
+        for (ReservaEntity ent : list) {
+            boolean found = false;
+            for (ReservaEntity entity : data) {
+                if (ent.getFecha().compareTo(entity.getFecha()) == 0) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+    
+    /**
+     * Prueba para eliminar una reserva.
+     */
+    @Test
+    public void deleteReservaTest() {
+        ReservaEntity entity = data.get(0);
+        reservaPersistence.delete(entity.getId());
+        ReservaEntity deleted = em.find(ReservaEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+    
+    /**
+     * Prueba para actualizar una reserva.
+     */
+    @Test
+    public void updateReservaTest() {
+        ReservaEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        ReservaEntity newEntity = factory.manufacturePojo(ReservaEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        reservaPersistence.update(newEntity);
+
+        ReservaEntity resp = em.find(ReservaEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getName(), resp.getName());
+    }
+}
