194016d72e6701b9493dcdfff73e8625e24d9ca3
Juan David Guzman Sarmiento <jd.guzman10@uniandes.edu.co>
2016-10-25 11:37:39 -0500

    biribbir

--- RestaurantesElSabor-web/src/main/java/co/edu/uniandes/rest/Restaurante/resources/RecursoCliente.java
+++ RestaurantesElSabor-web/src/main/java/co/edu/uniandes/rest/Restaurante/resources/RecursoCliente.java
@@ -5,11 +5,18 @@
  */
 package co.edu.uniandes.rest.Restaurante.resources;
 
+import co.edu.uniandes.bsod.restauranteselsabor.ejbs.ClienteLogic;
+import co.edu.uniandes.bsod.restauranteselsabor.entities.ClienteEntity;
+import co.edu.uniandes.bsod.restauranteselsabor.entities.MedioPagoEntity;
+import co.edu.uniandes.bsod.restauranteselsabor.entities.TarjetaPuntosEntity;
 import co.edu.uniandes.rest.Restaurante.mocks.MockClientes;
 import co.edu.uniandes.rest.Restaurante.dtos.ClienteDTO;
+import co.edu.uniandes.rest.Restaurante.dtos.MedioPagoDTO;
+import co.edu.uniandes.rest.Restaurante.dtos.TarjetaPuntosDTO;
 import co.edu.uniandes.rest.Restaurante.exceptions.LogicaRestauranteException;
+import java.util.ArrayList;
+import java.util.Date;
 
-//mport co.edu.uniandes.bsod.restauranteselsabor.ejbs.ClienteLogic;
 
 import java.util.List;
 import javax.ws.rs.Consumes;
@@ -30,9 +37,7 @@ import javax.ws.rs.Produces;
 @Consumes("application/json")
 public class RecursoCliente 
 {
-    MockClientes mockClientes = new MockClientes();
-    
-    //ClienteLogic logica = new ClienteLogic();
+    ClienteLogic logica = new ClienteLogic();
     /**
      * Retorna la lista de clientes.
      * @return lista de clientes.
@@ -41,8 +46,29 @@ public class RecursoCliente
     @GET
     public List<ClienteDTO> getClientes() throws LogicaRestauranteException 
     {
-        
-        return mockClientes.darClientes();
+        List<ClienteEntity> clientesLogica = logica.getClientes();
+        ArrayList<ClienteDTO> clientesWeb = new ArrayList<>();
+        for(ClienteEntity clienteLogica: clientesLogica)
+        {
+            Long id = clienteLogica.getId();
+            int documentoIdentidad = clienteLogica.getDocumentoIdentidad();
+            String tipoDocumentoIdentidad = clienteLogica.getTipoDocumentoIdentidad();
+            String name = clienteLogica.getName();
+            String apellidos = clienteLogica.getApellidos();
+            String direccion = clienteLogica.getDireccion();
+            int telefono = clienteLogica.getTelefono();
+            
+            TarjetaPuntosEntity tarjetaPuntosLogica = clienteLogica.getTarjetaPuntos();
+            Long idTarjetaPuntos = tarjetaPuntosLogica.getId();
+            Date fechaCaducidadTarjetaPuntos = tarjetaPuntosLogica.getFechaCaducidad();
+            int acumuladoTarjetaPuntos = tarjetaPuntosLogica.getAcumulado();
+            TarjetaPuntosDTO tarjetaPuntosWeb = new TarjetaPuntosDTO(idTarjetaPuntos, fechaCaducidadTarjetaPuntos, acumuladoTarjetaPuntos);
+            
+            List<MedioPagoEntity> mediosPagoLogica = clienteLogica.getMediosPago();
+            ArrayList<MedioPagoDTO> mediosPagoWeb = new ArrayList<>();
+            ClienteDTO clienteWeb = new ClienteDTO(id, documentoIdentidad, tipoDocumentoIdentidad, name, apellidos, direccion, telefono, tarjetaPuntosWeb, mediosPagoWeb);
+        }
+        return clientesWeb;
     }
     
      /**
@@ -55,7 +81,7 @@ public class RecursoCliente
     @Path("{idCliente}")
     public ClienteDTO getCliente(@PathParam("idCliente") Long pId) throws LogicaRestauranteException 
     {
-        return mockClientes.darCliente(pId);
+        return null;
     }
     
      /**
@@ -67,7 +93,7 @@ public class RecursoCliente
     @POST
     public ClienteDTO createCliente(ClienteDTO pNuevoCliente) throws LogicaRestauranteException
     {
-        return mockClientes.crearCliente(pNuevoCliente);
+        return null;
     }
     
     /**
@@ -78,7 +104,7 @@ public class RecursoCliente
     @PUT
     public ClienteDTO updateCliente(ClienteDTO pClienteAActualizar) throws LogicaRestauranteException 
     {
-        return mockClientes.actualizarCliente(pClienteAActualizar);
+        return null;
     }
     
     /**
@@ -90,7 +116,7 @@ public class RecursoCliente
     @Path("{idCliente}")
     public void deleteCliente(@PathParam("idCliente") Long pId) throws LogicaRestauranteException 
     {
-        mockClientes.eliminarCliente(pId);
+       
     }
     
 }
