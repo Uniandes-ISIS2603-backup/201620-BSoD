8d71bfedea69df15657cdae68038c308a3144a0a
Juan David Guzman Sarmiento <jd.guzman10@uniandes.edu.co>
2016-10-25 11:41:36 -0500

    dds

--- RestaurantesElSabor-back/src/test/java/co/edu/uniandes/restauranteselsabor/test/logic/ClienteLogicTest.java
+++ RestaurantesElSabor-back/src/test/java/co/edu/uniandes/restauranteselsabor/test/logic/ClienteLogicTest.java
@@ -5,37 +5,12 @@
  */
 package co.edu.uniandes.restauranteselsabor.test.logic;
 
-import co.edu.uniandes.bsod.restauranteselsabor.ejbs.ClienteLogic;
-import co.edu.uniandes.bsod.restauranteselsabor.entities.ClienteEntity;
-import co.edu.uniandes.bsod.restauranteselsabor.persistence.ClientePersistence;
-import java.util.ArrayList;
-import java.util.List;
-import javax.inject.Inject;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.runner.RunWith;
 
 /**
  *
  * @author jdguz
  */
-@RunWith(Arquillian.class)
 public class ClienteLogicTest 
 {
-    @Deployment
-    public static JavaArchive createDeployment() 
-    {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addPackage(ClienteEntity.class.getPackage())
-                .addPackage(ClientePersistence.class.getPackage())
-                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
-                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
-    }
     
-    @Inject
-    private ClienteLogic logic;
-    
-    private List<ClienteEntity> data = new ArrayList<ClienteEntity>();
 }
