11950044b97bcc1866486ddaff9555aacdc2ead5
Juan David Guzman Sarmiento <jd.guzman10@uniandes.edu.co>
2016-10-27 12:11:45 -0500

    dsfsdfsdf

--- RestaurantesElSabor-back/src/test/java/co/edu/uniandes/restauranteselsabor/test/logic/ClienteLogicTest.java
+++ RestaurantesElSabor-back/src/test/java/co/edu/uniandes/restauranteselsabor/test/logic/ClienteLogicTest.java
@@ -5,12 +5,119 @@
  */
 package co.edu.uniandes.restauranteselsabor.test.logic;
 
+import co.edu.uniandes.bsod.restauranteselsabor.api.IClienteLogic;
+import co.edu.uniandes.bsod.restauranteselsabor.api.ITarjetaPuntosLogic;
+import co.edu.uniandes.bsod.restauranteselsabor.ejbs.ClienteLogic;
+import co.edu.uniandes.bsod.restauranteselsabor.ejbs.TarjetaPuntosLogic;
+import co.edu.uniandes.bsod.restauranteselsabor.entities.ClienteEntity;
+import co.edu.uniandes.bsod.restauranteselsabor.entities.TarjetaPuntosEntity;
+import co.edu.uniandes.bsod.restauranteselsabor.persistence.ClientePersistence;
+import co.edu.uniandes.bsod.restauranteselsabor.persistence.TarjetaPuntosPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
 
 /**
  *
  * @author jdguz
  */
+@RunWith(Arquillian.class)
 public class ClienteLogicTest 
 {
+    private PodamFactory factory = new PodamFactoryImpl();
+    
+    @Inject
+    private IClienteLogic clienteLogic;
+    
+    @Inject
+    private ClientePersistence clientePersistence;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Inject
+    private UserTransaction utx;
+    
+    private List<ClienteEntity> data = new ArrayList<ClienteEntity>();
+ 
+    @Deployment
+    public static JavaArchive createDeployment() 
+    {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(ClienteEntity.class.getPackage())
+                .addPackage(ClienteLogic.class.getPackage())
+                .addPackage(IClienteLogic.class.getPackage())
+                .addPackage(ClientePersistence.class.getPackage())
+                .addPackage(TarjetaPuntosEntity.class.getPackage())
+                .addPackage(TarjetaPuntosPersistence.class.getPackage())
+                .addPackage(TarjetaPuntosLogic.class.getPackage())
+                .addPackage(ITarjetaPuntosLogic.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    @Before
+    public void setUp() 
+    {
+        try 
+        {
+            utx.begin();
+            clearData();
+            insertData();
+            utx.commit();
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+            try 
+            {
+                utx.rollback();
+            }
+            catch (Exception e1)
+            {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    private void clearData() 
+    {
+        em.createQuery("delete from TarjetaPuntosEntity").executeUpdate();
+        em.createQuery("delete from TarjetaPuntosEntity").executeUpdate();
+    }    
+    
+    private void insertData() 
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            ClienteEntity entity = factory.manufacturePojo(ClienteEntity.class);
+//            TarjetaPuntosEntity d = factory.manufacturePojo(TarjetaPuntosEntity.class);
+//            d.setCliente(entity);
+//            entity.setTarjetaPuntos(d);
+//            em.persist(entity);
+            data.add(entity);
+        }
+    }
+    
+    @Test
+    public void createClienteTest()
+    {
+        assertEquals(1, 1);
+    }
     
 }
