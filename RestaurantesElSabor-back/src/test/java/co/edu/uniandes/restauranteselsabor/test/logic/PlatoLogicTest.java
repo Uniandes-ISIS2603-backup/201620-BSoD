972249665a8bff2787a6fe1bfeea166456fea1f5
Zulma Castañeda <zl.castaneda10@uniandes.edu.co>
2016-10-27 11:17:02 -0500

    inicio  test del plato

---
+++ RestaurantesElSabor-back/src/test/java/co/edu/uniandes/restauranteselsabor/test/logic/PlatoLogicTest.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.restauranteselsabor.test.logic;
+
+/**
+ *
+ * @author zl.castaneda10
+ */
+public class PlatoLogicTest {
+    
+}
